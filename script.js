3b3682db77e5fbdf85ac968c079d39d553db1296
binary_soup <contact@binarysoup.dev>
2026-08-25 22:28:11 -0400

    Make single series

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var toggles = document.getElementsByClassName("list-toggle");
 
 for (var i = 0; i < toggles.length; i++) {
   toggles[i].addEventListener("click", function() {
-    this.parentElement.querySelector(".content").classList.toggle("active");
+    this.parentElement.querySelector(".collapsable").classList.toggle("show");
   });
 } 
