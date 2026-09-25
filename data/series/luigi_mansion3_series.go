dfae48df856a7a78b5eb802126b0d96929feccf9
binary_soup <contact@binarysoup.dev>
2026-09-24 20:31:41 -0400

    Re-index luigi mansion 3

--- data/series/luigi_mansion3_series.go
+++ data/series/luigi_mansion3_series.go
@@ -18,17 +18,17 @@ func (LuigiMansion3Series) GetName() string {
 	return "luigi_mansion3/series"
 }
 
+func (s LuigiMansion3Series) BuildEntryFromVideo(index int, video string, entry *build.Entry) error {
+	entry.Title = fmt.Sprintf("%s | ", s.calcFloor(index))
+	return nil
+}
+
 func (s LuigiMansion3Series) BuildEntryFromYoutube(_, videoIndex int, video *youtube.Video, entry *build.Entry) error {
 	title := strings.SplitN(video.Snippet.Title, " | ", 2)
 	if len(title) < 2 {
 		return errors.Format("invalid title")
 	}
-
-	if videoIndex == 0 {
-		entry.Title = fmt.Sprintf("Prelude | %s", util.Capitalize(title[0]))
-	} else {
-		entry.Title = fmt.Sprintf("Floor %s | %s", s.calcFloor(videoIndex), util.Capitalize(title[0]))
-	}
+	entry.Title = fmt.Sprintf("%s | %s", s.calcFloor(videoIndex+1), util.Capitalize(title[0]))
 
 	description := strings.Split(video.Snippet.Description, "\n")
 	if len(description) < 3 {
@@ -42,10 +42,12 @@ func (s LuigiMansion3Series) BuildEntryFromYoutube(_, videoIndex int, video *you
 func (LuigiMansion3Series) calcFloor(i int) string {
 	switch i {
 	case 1:
-		return "B1"
+		return "Prelude"
 	case 2:
-		return "05"
+		return "Floor B1"
+	case 3:
+		return "Floor 05"
 	default:
-		return "XX"
+		return "Floor XX"
 	}
 }
